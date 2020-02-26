3a763d7c8386cc17c43af6def2d439ec5f534d18
Harley McDowell <hmcdowell95@gmail.com>
2020-02-26 20:43:13 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,7 +1,13 @@
-# require libraries/modules here
+require "nokogiri"
+require 'pry'
+
 
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   
   kickstarter = Nokogiri::HTML(html)
+  
+  
 end
+
+create_project_hash
